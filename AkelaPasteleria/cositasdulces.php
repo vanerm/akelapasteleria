bca389f458ae20096ca325e588683c5dd9e07c78
Unknown <lopezseoane.f@gmail.com>
2017-09-13 20:19:40 -0300

    style mortal
    el footer queda anclado a la base

--- AkelaPasteleria/cositasdulces.php
+++ AkelaPasteleria/cositasdulces.php
@@ -2,6 +2,67 @@
 $title = 'Cositas Dulces';
 require('templates/open.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <body class="bodydulces">
+    <p> hola aca van a ir varias imagenes de las cositas dulces>..</p>
 
+		<!-- productos -->
+		<section class="vip-products">
+			<article class="product">
+				<img src="images/browny.jpg" alt="Responsive image" class="img-thumbnail">
+				<h2>Torta de Chocolate</h2>
+			</article>
+			<article class="product">
+				<img src="images/tarta_chocolate.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+			<article class="product">
+				<img src="images/cheescake.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+			<article class="product">
+				<img src="images/alfajores.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+			<article class="product">
+				<img src="images/torta_frutilla.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+			<article class="product">
+				<img src="images/choco_macarons.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+      <article class="product">
+				<img src="images/cuadrados_limon.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+      <article class="product">
+				<img src="images/berries.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+      <article class="product">
+				<img src="images/marquise.png" alt="Responsive image" class="img-thumbnail">
+				<h2>Lorem ipsum amet</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut recusandae eaque debitis sint necessitatibus, officia ex.</p>
+				<a href="#">ver más</a>
+			</article>
+		</section>
+  </body>
+</html>
 
 <?php require('templates/close.php'); ?>
